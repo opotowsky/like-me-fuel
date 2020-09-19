0171a3db96ead063783f50aa4a0f96d3abc35f8d
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2020-09-19 11:31:04 -0500

    oops, forgot the executable needed to be updated too

---
+++ mll_calc/htc_files/pca_run_python.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# un-tar python and packages
+tar -xzf python36.tar.gz
+tar -xzf packages.tar.gz
+
+# set path, pythonpath, and working directory as home
+export PATH=$PWD/python/bin:$PATH
+export PYTHONPATH=$PWD/packages
+export HOME=$PWD
+
+# run your script
+python3 pca_mll_pred.py $1 $2 $3 $4 $5 $6 $7 $8 $9 
