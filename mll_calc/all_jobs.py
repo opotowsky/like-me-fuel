845885d5933c2cf666bd8c618e3749b9df838989
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2021-06-17 23:06:42 -0400

    readying for new params.txt files

--- mll_calc/all_jobs.py
+++ mll_calc/all_jobs.py
@@ -1,10 +1,20 @@
 #! /usr/bin/env python3
 
-uncs = [0.0,]# 0.1, 0.15, 0.2]
-job_dirs = ['Job1_unc0.0',]
+# detectors
+#uncs = [0.0,]
+#job_dirs = ['Job2_unc0.0',]
+
+# activities
 #uncs = [0.05,]
-#job_dirs = ['Job0_unc0.05',]
+#job_dirs = ['Job2_unc0.05',]
+
+# nuclide masses
+#uncs = [0.01, 0.05, 0.1, 0.15, 0.2]
 #job_dirs = ['Job' + str(i) + '_unc' + str(unc) for i, unc in enumerate(uncs)]
+
+# sfcompo
+uncs = [0.01,]
+job_dirs = ['Job0_unc0.01',]
 
 
 kid_jobs = {'job_dirs' : job_dirs,
@@ -32,89 +42,89 @@ train_act32 = {'parent_dir' : 'train_act32',
                'test_pkl' : 'null'
                }
 
-d1_n113 = {'parent_dir' : 'train_d1_hpge_n113',
+d1_long = {'parent_dir' : 'train_d1_hpge_long',
            'ext_test' : '--no-ext-test',
            'ratios' : '--no-ratios',
            'train_pkl' : 'd1_hpge_spectra_113peaks_trainset.pkl',
            'test_pkl' : 'null'
            }
 
-d2_n113 = {'parent_dir' : 'train_d2_hpge_n113',
+d2_long = {'parent_dir' : 'train_d2_hpge_long',
            'ext_test' : '--no-ext-test',
            'ratios' : '--no-ratios',
            'train_pkl' : 'd2_hpge_spectra_113peaks_trainset.pkl',
            'test_pkl' : 'null'
            }
 
-d3_n113 = {'parent_dir' : 'train_d3_czt_n113',
+d3_long = {'parent_dir' : 'train_d3_czt_long',
            'ext_test' : '--no-ext-test',
            'ratios' : '--no-ratios',
            'train_pkl' : 'd3_czt_spectra_113peaks_trainset.pkl',
            'test_pkl' : 'null'
            }
 
-d4_n113 = {'parent_dir' : 'train_d4_nai_n113',
+d4_long = {'parent_dir' : 'train_d4_nai_long',
            'ext_test' : '--no-ext-test',
            'ratios' : '--no-ratios',
            'train_pkl' : 'd4_nai_spectra_113peaks_trainset.pkl',
            'test_pkl' : 'null'
            }
 
-d5_n113 = {'parent_dir' : 'train_d5_labr3_n113',
+d5_long = {'parent_dir' : 'train_d5_labr3_long',
            'ext_test' : '--no-ext-test',
            'ratios' : '--no-ratios',
            'train_pkl' : 'd5_labr3_spectra_113peaks_trainset.pkl',
            'test_pkl' : 'null'
            }
 
-d6_n113 = {'parent_dir' : 'train_d6_sri2_n113',
+d6_long = {'parent_dir' : 'train_d6_sri2_long',
            'ext_test' : '--no-ext-test',
            'ratios' : '--no-ratios',
            'train_pkl' : 'd6_sri2_spectra_113peaks_trainset.pkl',
            'test_pkl' : 'null'
            }
 
-d1_n31 = {'parent_dir' : 'train_d1_hpge_n31',
-          'ext_test' : '--no-ext-test',
-          'ratios' : '--no-ratios',
-          'train_pkl' : 'd1_hpge_spectra_31peaks_trainset.pkl',
-          'test_pkl' : 'null'
-          }
+d1_short = {'parent_dir' : 'train_d1_hpge_short',
+            'ext_test' : '--no-ext-test',
+            'ratios' : '--no-ratios',
+            'train_pkl' : 'd1_hpge_spectra_31peaks_trainset.pkl',
+            'test_pkl' : 'null'
+            }
 
-d2_n31 = {'parent_dir' : 'train_d2_hpge_n31',
-          'ext_test' : '--no-ext-test',
-          'ratios' : '--no-ratios',
-          'train_pkl' : 'd2_hpge_spectra_31peaks_trainset.pkl',
-          'test_pkl' : 'null'
-          }
+d2_short = {'parent_dir' : 'train_d2_hpge_short',
+            'ext_test' : '--no-ext-test',
+            'ratios' : '--no-ratios',
+            'train_pkl' : 'd2_hpge_spectra_31peaks_trainset.pkl',
+            'test_pkl' : 'null'
+            }
 
-d3_n31 = {'parent_dir' : 'train_d3_czt_n31',
-          'ext_test' : '--no-ext-test',
-          'ratios' : '--no-ratios',
-          'train_pkl' : 'd3_czt_spectra_31peaks_trainset.pkl',
-          'test_pkl' : 'null'
-          }
+d3_short = {'parent_dir' : 'train_d3_czt_short',
+            'ext_test' : '--no-ext-test',
+            'ratios' : '--no-ratios',
+            'train_pkl' : 'd3_czt_spectra_31peaks_trainset.pkl',
+            'test_pkl' : 'null'
+            }
 
-d4_n31 = {'parent_dir' : 'train_d4_nai_n31',
-          'ext_test' : '--no-ext-test',
-          'ratios' : '--no-ratios',
-          'train_pkl' : 'd4_nai_spectra_31peaks_trainset.pkl',
-          'test_pkl' : 'null'
-          }
+d4_short = {'parent_dir' : 'train_d4_nai_short',
+            'ext_test' : '--no-ext-test',
+            'ratios' : '--no-ratios',
+            'train_pkl' : 'd4_nai_spectra_31peaks_trainset.pkl',
+            'test_pkl' : 'null'
+            }
 
-d5_n31 = {'parent_dir' : 'train_d5_labr3_n31',
-          'ext_test' : '--no-ext-test',
-          'ratios' : '--no-ratios',
-          'train_pkl' : 'd5_labr3_spectra_31peaks_trainset.pkl',
-          'test_pkl' : 'null'
-          }
+d5_short = {'parent_dir' : 'train_d5_labr3_short',
+            'ext_test' : '--no-ext-test',
+            'ratios' : '--no-ratios',
+            'train_pkl' : 'd5_labr3_spectra_31peaks_trainset.pkl',
+            'test_pkl' : 'null'
+            }
 
-d6_n31 = {'parent_dir' : 'train_d6_sri2_n31',
-          'ext_test' : '--no-ext-test',
-          'ratios' : '--no-ratios',
-          'train_pkl' : 'd6_sri2_spectra_31peaks_trainset.pkl',
-          'test_pkl' : 'null'
-          }
+d6_short = {'parent_dir' : 'train_d6_sri2_short',
+            'ext_test' : '--no-ext-test',
+            'ratios' : '--no-ratios',
+            'train_pkl' : 'd6_sri2_spectra_31peaks_trainset.pkl',
+            'test_pkl' : 'null'
+            }
 
 d1_auto = {'parent_dir' : 'train_d1_hpge_auto',
            'ext_test' : '--no-ext-test',
@@ -158,12 +168,12 @@ d6_auto = {'parent_dir' : 'train_d6_sri2_auto',
            'test_pkl' : 'null'
            }
 
-train_29 = {'parent_dir' : 'train29',
-            'ext_test' : '--no-ext-test',
-            'ratios' : '--no-ratios',
-            'train_pkl' : 'sim_grams_nuc29.pkl',
-            'test_pkl' : 'sfcompo_nuc29.pkl'
-            }
+train_nuc29 = {'parent_dir' : 'train_nuc29',
+               'ext_test' : '--no-ext-test',
+               'ratios' : '--no-ratios',
+               'train_pkl' : 'sim_grams_nuc29.pkl',
+               'test_pkl' : 'null'
+               }
 
 sfco_10ratio = {'parent_dir' : 'sfco10ratio',
                 'ext_test' : '--ext-test',
@@ -172,12 +182,12 @@ sfco_10ratio = {'parent_dir' : 'sfco10ratio',
                 'test_pkl' : 'sfcompo_nuc15.pkl'
                 }
 
-sfco_29 = {'parent_dir' : 'sfco29',
-           'ext_test' : '--ext-test',
-           'ratios' : '--no-ratios',
-           'train_pkl' : 'sim_grams_nuc29.pkl',
-           'test_pkl' : 'sfcompo_nuc29.pkl'
-           }
+sfco_nuc29 = {'parent_dir' : 'sfco_nuc29',
+              'ext_test' : '--ext-test',
+              'ratios' : '--no-ratios',
+              'train_pkl' : 'sim_grams_nuc29.pkl',
+              'test_pkl' : 'sfcompo_nuc29.pkl'
+              }
 
 train_pca29 = {'parent_dir' : 'pca_train29',
                'ext_test' : '--no-ext-test',
@@ -217,4 +227,9 @@ bad_sfco = {'parent_dir' : 'sfco_bad',
             'test_pkl' : 'sfco_bad.pkl'
             }
 
-parent_jobs = [d1_auto, d2_auto, d3_auto, d4_auto, d5_auto, d6_auto]
+#parent_jobs = [d1_auto, d2_auto, d3_auto, d4_auto, d5_auto, d6_auto,
+#               d1_short, d2_short, d3_short, d4_short, d5_short, d6_short,
+#               d1_long, d2_long, d3_long, d4_long, d5_long, d6_long]
+#parent_jobs = [train_act4, train_act9, train_act32]
+#parent_jobs = [train_nuc29,]
+parent_jobs = [sfco_nuc29,]
